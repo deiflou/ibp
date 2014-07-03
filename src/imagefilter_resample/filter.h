c51fb72243ec8d771e6bfad401e9f8ef90aa22f6
Deif Lou <ginoba@gmail.com>
2014-07-03 11:44:58 +0200

    * Added Resample image filter

---
+++ src/imagefilter_resample/filter.h
@@ -0,0 +1,84 @@
+/****************************************************************************
+**
+** Copyright (C) 2014 Deif Lou
+**
+** This file is part of Anitools
+**
+** Anitools is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**
+****************************************************************************/
+
+#ifndef FILTER_H
+#define FILTER_H
+
+#include <QObject>
+#include <QHash>
+#include <QString>
+#include <QImage>
+#include <QSettings>
+#include <QWidget>
+
+#include "../imgproc/imagefilter.h"
+
+using namespace anitools::imgproc;
+
+class Filter : public ImageFilter
+{
+    Q_OBJECT
+
+public:
+    enum SizeMode
+    {
+        Percent,
+        Pixels,
+        KeepAspectRatio
+    };
+    enum ResamplingMode
+    {
+        NearestNeighbor,
+        Bilinear,
+        Bicubic,
+        Lanczos
+    };
+
+    Filter();
+    ~Filter();
+    ImageFilter * clone();
+    QHash<QString, QString> info();
+    QImage process(const QImage & inputImage);
+    bool loadParameters(QSettings & s);
+    bool saveParameters(QSettings & s);
+    QWidget * widget(QWidget *parent = 0);
+
+private:
+    int mWidth, mHeight;
+    SizeMode mWidthMode, mHeightMode;
+    ResamplingMode mResamplingMode;
+
+signals:
+    void widthChanged(int w);
+    void heightChanged(int h);
+    void widthModeChanged(Filter::SizeMode wm);
+    void heightModeChanged(Filter::SizeMode hm);
+    void resamplingModeChanged(Filter::ResamplingMode rm);
+
+public slots:
+    void setWidth(int w);
+    void setHeight(int h);
+    void setWidthMode(Filter::SizeMode wm);
+    void setHeightMode(Filter::SizeMode hm);
+    void setResamplingMode(Filter::ResamplingMode rm);
+};
+
+#endif // FILTER_H
---
+++ src/imagefilter_resample/main.cpp
@@ -0,0 +1,53 @@
+/****************************************************************************
+**
+** Copyright (C) 2014 Deif Lou
+**
+** This file is part of Anitools
+**
+** Anitools is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**
+****************************************************************************/
+
+#include <QHash>
+
+#include "filter.h"
+#include "../imgproc/imagefilter.h"
+
+using namespace anitools::imgproc;
+
+#ifdef Q_OS_WIN32
+#define ANITOOLS_EXPORT __declspec(dllexport)
+#else
+#define ANITOOLS_EXPORT
+#endif
+
+extern "C" ANITOOLS_EXPORT QHash<QString, QString> getAnitoolsPluginInfo()
+{
+    QHash<QString, QString> info;
+
+    info.insert("id", "anitools.imagefilter.resample");
+    info.insert("version", "0.1.0");
+    info.insert("name", QObject::tr("Resample"));
+    info.insert("description", QObject::tr("Resample the image to a new size using the selected resampling interpolation mode"));
+    info.insert("tags", QObject::tr("Geometry"));
+    info.insert("author", QObject::tr("Deif Lou"));
+    info.insert("copyright", QObject::tr(""));
+    info.insert("url", QObject::tr(""));
+    return info;
+}
+
+extern "C" ANITOOLS_EXPORT ImageFilter * getImageFilterInstance()
+{
+    return new Filter();
+}
