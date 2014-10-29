84aa993b5f6f29ef72456833ffd6d39fc88211cb
Deif Lou <ginoba@gmail.com>
2014-10-29 13:19:32 +0100

    Added Auto Trim image filter

---
+++ src/imagefilter_autotrim/filter.h
@@ -0,0 +1,72 @@
+/****************************************************************************
+**
+** Copyright (C) 2014 Deif Lou
+**
+** This file is part of Anitools
+**
+** Anitools is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**
+****************************************************************************/
+
+#ifndef FILTER_H
+#define FILTER_H
+
+#include <QObject>
+#include <QHash>
+#include <QString>
+#include <QImage>
+#include <QSettings>
+#include <QWidget>
+
+#include "../imgproc/imagefilter.h"
+
+using namespace anitools::imgproc;
+
+class Filter : public ImageFilter
+{
+    Q_OBJECT
+
+public:
+    enum Reference
+    {
+        AlphaChannel,
+        Luma
+    };
+
+    Filter();
+    ~Filter();
+    ImageFilter * clone();
+    QHash<QString, QString> info();
+    QImage process(const QImage & inputImage);
+    bool loadParameters(QSettings & s);
+    bool saveParameters(QSettings & s);
+    QWidget * widget(QWidget *parent = 0);
+
+private:
+    Reference mReference;
+    int mThreshold;
+    QMargins mMargins;
+
+signals:
+    void referenceChanged(Filter::Reference v);
+    void thresholdChanged(int v);
+    void marginsChanged(const QMargins & v);
+
+public slots:
+    void setReference(Filter::Reference v);
+    void setThreshold(int v);
+    void setMargins(const QMargins & v);
+};
+
+#endif // FILTER_H
